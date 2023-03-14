48da22b903bbbb57d0face69771033d3e1c59902
RalfKonrad <github@eckel-consulting.de>
2023-03-14 22:53:21 +0100

    Included and linked benchmark to src/ql_performance_testing.cpp

--- src/ql_performance_testing.cpp
+++ src/ql_performance_testing.cpp
@@ -1,8 +1,8 @@
-// #include <benchmark/benchmark.h>
+#include <benchmark/benchmark.h>
 #include <iostream>
 #include <ql/time/date.hpp>
 
 int main() {
   auto date = QuantLib::Date::todaysDate();
-  std::cout << "hello world at " << date << std::endl;
+  std::cout << "hello world at " << date << "." << std::endl;
 }
