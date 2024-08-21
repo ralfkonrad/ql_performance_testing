89510d6ca927f91f64f7ea9ab54e5b33cae2c1a4
ralfkonrad <github@eckel-consulting.de>
2024-08-21 17:29:56 +0200

    Disable warnings for Google's Benchmark

--- cmake/WarningLevels.cmake
+++ cmake/WarningLevels.cmake
@@ -8,18 +8,6 @@ else ()
 endif ()
 
 # Specify whether to treat warnings on compile as errors.
-if (RKE_COMPILE_WARNING_AS_ERROR)
-  # all compiler warnings as errors
-  if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
-    # since v3.24 cmake can set all compiler warnings as errors itself
-    set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
-  else ()
-    # or set them manually
-    if (MSVC)
-      add_compile_options(-WX)
-    else ()
-      add_compile_options(-Werror)
-    endif ()
-  endif ()
+if (QL_COMPILE_WARNING_AS_ERROR)
+  set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
 endif ()
-
