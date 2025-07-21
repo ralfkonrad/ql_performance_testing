0bae06b4dfc366c5e07b7f5063424dfc11916c40
RalfKonrad <github@eckel-consulting.de>
2025-07-21 16:20:26 +0200

    Enable clang-tidy

--- benchmark/ql_calendar_testing.cpp
+++ benchmark/ql_calendar_testing.cpp
@@ -7,16 +7,16 @@
 #include <vector>
 
 void BM_TestCalendar(benchmark::State& state) {
-  for (auto _ : state) {
-    auto target = QuantLib::TARGET();
+    for (auto _ : state) { // NOLINT(clang-analyzer-deadcode.DeadStores)
+        auto target = QuantLib::TARGET();
 
-    auto numberOfDates = QuantLib::Date::maxDate() - QuantLib::Date::minDate() + 1;
-    auto isBusinessDate = std::vector<bool>(numberOfDates);
+        auto numberOfDates = QuantLib::Date::maxDate() - QuantLib::Date::minDate() + 1;
+        auto isBusinessDate = std::vector<bool>(numberOfDates);
 
-    auto i = 0;
-    for (auto date = QuantLib::Date::minDate(); date < QuantLib::Date::maxDate();) {
-      isBusinessDate[i++] = target.isBusinessDay(date);
-      date++;
+        auto i = 0;
+        for (auto date = QuantLib::Date::minDate(); date < QuantLib::Date::maxDate();) {
+            isBusinessDate[i++] = target.isBusinessDay(date);
+            date++;
+        }
     }
-  }
 }
